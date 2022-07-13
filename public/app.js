67d878b787e794748450de867a2a200e5159336a
davblight <davblight@gmail.com>
2022-07-13 09:39:47 -0600

    frontend auth hookup started

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+const URL = "http://localhost:8080"
+
 var app = new Vue({
     el: "#app",
     data: {
@@ -32,8 +34,81 @@ var app = new Vue({
         showSettings: function () {
             this.subpage = "settings";
         },
-        loginFunction: function () {
-            this.showHome();
+
+        //USER LOGIN AND AUTHENTICATION LOGIC
+        //First, see if we're logged in
+        getSession: async function () {
+            let response = await fetch(`${URL}/session`, {
+                credentials: "include"
+            });
+            if (response.status == 200) {
+                console.log("logged in");
+                let data = await response.json();
+                console.log(data);
+                this.page = "home";
+            } else if (response.status == 401) {
+                console.log("not logged in");
+                let data = await response.json();
+                console.log(data);
+            } else {
+                console.log("Something went wrong while getting /session", response.status, response)
+            }
         },
+        //Attempt to log in
+        postSession: async function () {
+            let loginCreds = {
+                username: this.usernameInput,
+                password: this.passwordInput
+            };
+            let response = await fetch(`${URL}/session`, {
+                method: "POST",
+                body: JSON.stringify(loginCreds),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            let body = response.json();
+            console.log(body);
+
+            if (response.status == 201) {
+                console.log("successful login attempt");
+                this.page = "home"
+            } else if (response.status = 401) {
+                console.log("unsuccessful login attempt");
+            } else {
+                console.log("something went wrong while posting /session", response.status, response)
+            }
+        },
+        //Create new user
+        postUser: async function () {
+            let newUser = { 
+                username: this.nameInput,
+                password: this.passwordInput
+            };
+            let response = await fetch(`${URL}/users`, {
+                method: "POST",
+                body: JSON.stringify(newUser),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+            
+            let body = response.json();
+            console.log(body);
+
+            if (response.status == 201) {
+                console.log("user successfully created");
+            } else if (response.status = 404) {
+                console.log("error creating user");
+            } else {
+                console.log("unknown error posting /users", response.status, response);
+            }
+        },
+    },
+    created: function () {
+        this.getSession();
     }
 })
