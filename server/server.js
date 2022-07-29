1c062b14a3e7fe700ec30ce427f924958a07fd2f
davblight <davblight@gmail.com>
2022-07-29 09:30:49 -0600

    trying to host

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 //host front end
-app.use(express.static(`${__dirname}/../public/`));
+app.use(express.static(`${__dirname}/../docs/`));
 
 setUpSession(app);
 setUpAuth(app);
